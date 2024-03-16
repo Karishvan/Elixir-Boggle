21030d7a1e9831cb4b86a5225aeec2555f61f94b
Karishvan Ragunathan <k1ragunathan@torontomu.ca>
2024-03-16 17:57:03 -0400

    half-working solution of recursiveDFS

--- lib/boggle.ex
+++ lib/boggle.ex
@@ -10,6 +10,30 @@ defmodule Boggle do
     :error  
   end
 
+  def recursiveDFS(board, [{x,y} | _ ], ""), do: []
+  def recursiveDFS(board, {x,y}, word), do: recursiveDFS(board, [{x,y} | getNeighbours({x,y}, board)], word)
+  def recursiveDFS(board, [], word), do: false
+  def recursiveDFS(board, [{x,y} | t], word) do
+   letter = elem(elem(board, x),y)
+     cond do
+      (letter == String.at(word, 0)) -> 
+        row = put_elem(elem(board, x), y, "*")
+        board = put_elem(board, x, row)
+        visit = recursiveDFS(board, {x,y}, String.slice(word, 1..-1//1))
+        path = false
+        cond do
+          visit != false -> path = [ {x,y} | visit]
+          true -> false
+        end
+        #path = [ {x,y} | recursiveDFS(board, {x,y}, String.slice(word, 1..-1//1)) ] #fix this
+        row = put_elem(elem(board, x), y, letter)
+        board = put_elem(board, x, row)
+        path
+      true -> 
+        recursiveDFS(board, t, word)
+    end
+  end
+
   def getNeighbours({x,y}, board) do
     adjacentTiles = [{x-1,y-1}, {x, y-1}, {x+1, y-1}, {x+1, y}, {x+1, y+1}, {x, y+1}, {x-1, y+1}, {x-1, y}]
     filterNeighbours(adjacentTiles, board, [])
